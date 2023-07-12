70205eb4ebc58cd0f4f2e26b0c725a9cb002e1b8
Jack Lingle <captainjack0818@gmail.com>
2023-07-12 19:05:24 -0400

    finish webpack config

--- webpack-react/src/App.js
+++ webpack-react/src/App.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './index.css';
 
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    axios.get(url).then((res) => setNotes(res.data));
+  }, [url])
+  return notes
+}
 const App = () => {
   const [counter, setCounter] = useState(0)
   const [values, setValues] = useState([]);
+  const url = BACKEND_URL;
+  const notes = useNotes(url);
   const handleClick = () => {
     setCounter(counter + 1);
     setValues(values.concat(counter));
@@ -14,6 +25,7 @@ const App = () => {
       <button onClick={handleClick}>
         press
       </button>
+      <div>{notes.length} notes on server {url}</div>
     </div>
   )
 }
