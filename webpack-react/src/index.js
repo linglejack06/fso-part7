1df95e4fcea94dfbdcc8032776a283fa5344f6bd
Jack Lingle <captainjack0818@gmail.com>
2023-07-12 18:50:40 -0400

    add self-made react app with webpack

---
+++ webpack-react/src/index.js
@@ -0,0 +1,8 @@
+import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'core-js/stable/index.js';
+import 'regenerator-runtime/runtime.js';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
