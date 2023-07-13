4e26c3ce86bc0642e818c480aa59206ee8958cbb
Jack Lingle <captainjack0818@gmail.com>
2023-07-13 17:20:38 -0400

    add prettier

--- blog-frontend/.eslintrc.cjs
+++ blog-frontend/.eslintrc.cjs
@@ -6,13 +6,15 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     "react-app",
-    "react-app/jest"
+    "react-app/jest",
+    "prettier"
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': 'warn',
     'react/prop-types': 'off',
+    'prettier/prettier': ['error'],
   },
 }
