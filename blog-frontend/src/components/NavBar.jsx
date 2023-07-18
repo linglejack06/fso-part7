d96951b7db9d7e3bb39dd750da654e7a3037cf08
Jack Lingle <captainjack0818@gmail.com>
2023-07-18 17:59:29 -0400

    finish styling of navbar component

--- blog-frontend/src/components/NavBar.jsx
+++ blog-frontend/src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import {
   removeUser,
   useUserDispatch,
@@ -8,24 +8,59 @@ import blogService from "../services/blogService";
 
 const NavBar = () => {
   const user = useUserValue();
+  const [full, setFull] = useState(false);
+  const path = useLocation();
   const userDispatch = useUserDispatch();
   const handleLogout = () => {
     blogService.setToken("");
     window.localStorage.removeItem("loggedUser");
     userDispatch(removeUser());
+    className = "";
   };
   return (
-    <div className="flex justify-between p-4">
-      <div className="flex flex-auto gap-8">
-        <Link to="/">Home</Link>
-        <Link to="/users">Users</Link>
-        {user ? <Link to="/create">Create Blog</Link> : null}
-      </div>
-      <div className="flex-auto">
+    <div className="mb-4 flex h-12 justify-between ring-4 ring-orange-400">
+      <div className="flex h-full flex-auto">
+        <Link
+          className={`${
+            path.pathname === "/" ? "text-orange-600" : ""
+          } text-md flex h-full items-center border-r-2 px-4 text-center font-semibold transition duration-200 hover:font-bold hover:text-purple-700 hover:underline hover:underline-offset-4`}
+          to="/"
+        >
+          Home
+        </Link>
+        <Link
+          className={`${
+            path.pathname === "/users" ? "text-orange-600" : ""
+          } text-md flex h-full items-center border-r-2 px-4 font-semibold transition duration-200 hover:font-bold hover:text-purple-700 hover:underline hover:underline-offset-4`}
+          to="/users"
+        >
+          Users
+        </Link>
         {user ? (
-          <div className="flex flex-auto justify-end gap-8">
-            <p>Logged in: {user.username}</p>
-            <button onClick={handleLogout}>Logout</button>
+          <Link
+            className={`${
+              path.pathname === "/create" ? "text-orange-600" : ""
+            } text-md flex h-full items-center border-r-2 px-4 font-semibold transition duration-200 hover:font-bold hover:text-purple-700 hover:underline hover:underline-offset-4`}
+            to="/create"
+          >
+            Create Blog
+          </Link>
+        ) : null}
+      </div>
+      <div className="mr-2 flex h-full flex-auto">
+        {user ? (
+          <div className="flex flex-auto items-center justify-end self-center">
+            <h3 className="text-md h-full px-4 text-center font-semibold">
+              Logged in: {user.username}
+            </h3>
+            <div className="flex h-full items-center">
+              <button
+                className="text-md h-7 rounded-lg bg-purple-600 px-4 text-center font-semibold text-white transition duration-200 hover:scale-105 hover:bg-purple-700 hover:font-bold"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+            </div>
           </div>
         ) : (
           <div className="flex flex-auto justify-end">
