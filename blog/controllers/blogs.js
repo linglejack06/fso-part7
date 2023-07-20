b6de180f013741629e726627f9b35e07151fef17
Jack Lingle <captainjack0818@gmail.com>
2023-07-20 18:05:12 -0400

    fic comment router to post

--- blog/controllers/blogs.js
+++ blog/controllers/blogs.js
@@ -1,18 +1,21 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-underscore-dangle */
-const blogRouter = require('express').Router();
-const Blog = require('../models/blog');
-const { userExtractor } = require('../utils/middleware');
+const blogRouter = require("express").Router();
+const Blog = require("../models/blog");
+const { userExtractor } = require("../utils/middleware");
 
-blogRouter.get('/', async (req, res, next) => {
+blogRouter.get("/", async (req, res, next) => {
   try {
-    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
+    const blogs = await Blog.find({}).populate("user", {
+      username: 1,
+      name: 1,
+    });
     res.json(blogs);
   } catch (error) {
     next(error);
   }
 });
-blogRouter.post('/', userExtractor, async (req, res, next) => {
+blogRouter.post("/", userExtractor, async (req, res, next) => {
   const { user } = req;
   try {
     let author;
@@ -31,13 +34,16 @@ blogRouter.post('/', userExtractor, async (req, res, next) => {
     const savedBlog = await blog.save();
     user.blogs = [...user.blogs, savedBlog._id];
     await user.save();
-    const populatedBlog = await savedBlog.populate('user', { username: 1, name: 1 });
+    const populatedBlog = await savedBlog.populate("user", {
+      username: 1,
+      name: 1,
+    });
     return res.status(201).json(populatedBlog);
   } catch (error) {
     return next(error);
   }
 });
-blogRouter.delete('/:id', userExtractor, async (req, res, next) => {
+blogRouter.delete("/:id", userExtractor, async (req, res, next) => {
   const { user } = req;
   try {
     const blog = await Blog.findById(req.params.id);
@@ -45,12 +51,14 @@ blogRouter.delete('/:id', userExtractor, async (req, res, next) => {
       await Blog.findByIdAndRemove(blog.id);
       return res.status(204).end();
     }
-    return res.status(401).json({ error: 'User does not have rights to delete blog' });
+    return res
+      .status(401)
+      .json({ error: "User does not have rights to delete blog" });
   } catch (error) {
     return next(error);
   }
 });
-blogRouter.put('/:id', async (req, res, next) => {
+blogRouter.put("/:id", async (req, res, next) => {
   const { body } = req;
   const blog = {
     author: body.author,
@@ -59,14 +67,19 @@ blogRouter.put('/:id', async (req, res, next) => {
     likes: body.likes,
   };
   try {
-    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, { new: true });
-    const populatedBlog = await updatedBlog.populate('user', { username: 1, name: 1 });
+    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, {
+      new: true,
+    });
+    const populatedBlog = await updatedBlog.populate("user", {
+      username: 1,
+      name: 1,
+    });
     res.json(populatedBlog);
   } catch (error) {
     next(error);
   }
 });
-blogRouter.put('/:id/comments', async (req, res, next) => {
+blogRouter.post("/:id/comments", async (req, res, next) => {
   const { comment } = req.body;
   try {
     const blogs = await Blog.find({});
@@ -75,9 +88,12 @@ blogRouter.put('/:id/comments', async (req, res, next) => {
     const updatedBlog = await Blog.findByIdAndUpdate(
       req.params.id,
       { comments: [...blogComments, comment] },
-      { new: true },
+      { new: true }
     );
-    const populatedBlog = await updatedBlog.populate('user', { username: 1, name: 1 });
+    const populatedBlog = await updatedBlog.populate("user", {
+      username: 1,
+      name: 1,
+    });
     res.json(populatedBlog);
   } catch (error) {
     next(error);
