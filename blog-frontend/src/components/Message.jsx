9acd8d2f0adf4004a0ae48fec86dede170c0e361
Jack Lingle <captainjack0818@gmail.com>
2023-07-22 17:27:38 -0400

    style alerts

--- blog-frontend/src/components/Message.jsx
+++ blog-frontend/src/components/Message.jsx
@@ -1,12 +1,23 @@
 import { useNotificationValue } from "../contexts/notificationContext";
+import { ReactComponent as Alert } from "../alert.svg";
+import { ReactComponent as Check } from "../check.svg";
 
 const Message = () => {
   const notification = useNotificationValue();
   if (notification.message === "") return null;
+  if (notification.error) {
+    return (
+      <div className="mx-auto flex h-8 w-5/6 justify-between rounded-lg bg-red-400 bg-opacity-90 p-4">
+        <Alert className="h-4 w-4 self-center" />
+        <h1>{notification.message}</h1>
+      </div>
+    );
+  }
   return (
-    <h1 className={notification.error ? "error message" : "validation message"}>
-      {notification.message}
-    </h1>
+    <div className=" mx-auto flex h-8 w-5/6 items-center justify-between rounded-lg bg-green-100 bg-opacity-90 p-4">
+      <Check className="h-4 w-4 self-center" />
+      <h1 className="text-lg">{notification.message}</h1>
+    </div>
   );
 };
 
